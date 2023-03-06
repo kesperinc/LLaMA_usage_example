4d019a2c180a9b076bf5484f3b43af40c536640f
noah.go <69115576+go-noah@users.noreply.github.com>
2023-03-07 05:12:13 +0900

    configuration for RTX 3090, RTX 4090

--- example.py
+++ example.py
@@ -2,13 +2,22 @@
 import llama
 
 MODEL = 'decapoda-research/llama-7b-hf'
-# MODEL = 'decapoda-research/llama-13b-hf'
-# MODEL = 'decapoda-research/llama-30b-hf'
-# MODEL = 'decapoda-research/llama-65b-hf'
 
 tokenizer = llama.LLaMATokenizer.from_pretrained(MODEL)
-model = llama.LLaMAForCausalLM.from_pretrained(MODEL, low_cpu_mem_usage = True)
+model = llama.LLaMAForCausalLM.from_pretrained(MODEL, low_cpu_mem_usage = True).half()
 model.to('cuda')
 
-batch = tokenizer("Yo mama", return_tensors = "pt", add_special_tokens = False)
+prompt = """Tweet: "I hate it when my phone battery dies."
+Sentiment: Negative
+###
+Tweet: "My day has been 👍"
+Sentiment: Positive
+###
+Tweet: "This is the link to the article"
+Sentiment: Neutral
+###
+Tweet: "This new music video was incredibile"
+Sentiment:"""
+
+batch = tokenizer(prompt, return_tensors = "pt", add_special_tokens = False)
 print(tokenizer.decode(model.generate(batch["input_ids"].cuda(), max_length=100)[0]))
